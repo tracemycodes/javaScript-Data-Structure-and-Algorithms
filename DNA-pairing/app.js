0ffc57891b11a50e29ba6c709e526750d468c1b2
Agba Emmanuel <agbaayoh@gmail.com>
2022-02-02 07:28:23 -0700

    Dna pairing

--- DNA-pairing/app.js
+++ DNA-pairing/app.js
@@ -1,1 +1,31 @@
 console.log("object");
+
+
+// AT and CG.
+
+function pairElement(str) {
+   let strArr = str.split(''),
+      pairedArr = [];
+
+  strArr.forEach(element => {
+    switch (element) {
+      case 'A':
+        pairedArr.push(['A', 'T'])
+        break;
+      case 'T':
+        pairedArr.push(['T', 'A'])
+        break;
+      case 'C':
+        pairedArr.push(['C', 'G'])
+        break;
+      case 'G':
+        pairedArr.push(['G', 'C'])
+        break;  
+      default:
+        break;
+    }
+  });
+  return pairedArr;
+}
+
+console.log(pairElement("GCG"))
