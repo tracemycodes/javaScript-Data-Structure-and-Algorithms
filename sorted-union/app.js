540e6167e376d35a0b6c29c0ec2e98659c04c6d5
Agba Emmanuel <agbaayoh@gmail.com>
2022-02-02 23:55:20 -0700

    sorted union

--- sorted-union/app.js
+++ sorted-union/app.js
@@ -1,5 +1,16 @@
-function uniteUnique(arr) {
-  clg
+function uniteUnique(agu) {
+  let arr = Array.from(arguments),
+  result = [];
+ 
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (!result.includes(arr[i][j])) {
+        result.push(arr[i][j])
+        console.log(result);
+      }
+    }    
+  }
+  return result
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]);
