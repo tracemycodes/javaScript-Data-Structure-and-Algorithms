c85498846bfb1884719ff902fdff450c60928978
Agba Emmanuel <agbaayoh@gmail.com>
2022-03-27 19:39:44 -0700

    caesar cipher algorithm solved

---
+++ caesars cipher/app.js
@@ -0,0 +1,40 @@
+// Caesars Cipher
+
+// One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+// A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+// All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+// rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+
+// rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+
+// rot13("SERR YBIR?") should decode to the string FREE LOVE?
+
+// rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+
+function rot13(str) {
+  let decResult = '';
+  let alphaStart = 'ABCDEFGHIJKLM';
+  let alphaEnd = 'NOPQRSTUVWXYZ';
+
+  for (let i = 0; i < str.length; i++) {
+    const element = str[i];
+
+    if (alphaStart.indexOf(element) >= 0) {
+      decResult += alphaEnd[alphaStart.indexOf(element)];
+    } else if (alphaEnd.indexOf(element) >= 0) {
+      decResult += alphaStart[alphaEnd.indexOf(element)];
+    } else {
+      decResult += element;
+    }
+  }
+
+  return decResult;
+}
+
+// Change the inputs below to test
+console.log(rot13('SERR PBQR PNZC'));
