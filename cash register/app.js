ebe96349189a309d9521c2fc886ca94e3ff725a4
Agba Emmanuel <agbaayoh@gmail.com>
2022-04-01 21:45:49 -0700

    successfully sloved the cash register algorithm

--- cash register/app.js
+++ cash register/app.js
@@ -38,53 +38,61 @@
 //   return a[1] - b[1];
 // })
 
+
+// converted all prices to integers for error management
 const currencyUnit = {
-  PENNY: 0.01,
-  NICKEL: 0.05,
-  DIME: 0.1,
-  QUARTER: 0.25,
-  ONE: 1,
-  FIVE: 5,
-  TEN: 10,
-  TWENTY: 20,
-  'ONE HUNDRED': 100,
+  PENNY: 1,
+  NICKEL: 5,
+  DIME: 10,
+  QUARTER: 25,
+  ONE: 100,
+  FIVE: 500,
+  TEN: 1000,
+  TWENTY: 2000,
+  'ONE HUNDRED': 10000,
 };
 
-// console.log(currencyUnit["ONE HUNDRED"]);
 
 function checkCashRegister(price, cash, cid) {
-  let changeSum = cash - price;
-  let changeSumCheck = changeSum;
+  let changeSum = (cash * 100) - (price * 100);
   let change = [];
   let status = '';
 
+
   let crSum = 0;
+
+  // filter to remove all current price denominations without funds
   let filterCid = cid.filter((cash) => cash[1] !== 0).reverse();
 
   filterCid.forEach((element) => {
     let currPrice = element[0];
-    crSum += element[1];
-
+    let multiAmt = element[1] * 100;
+    crSum += multiAmt;
     let amount = 0;
-    while (changeSum >= currencyUnit[currPrice] && crSum > 0) {
+
+    while (changeSum >= currencyUnit[currPrice] && crSum > 0 && amount < multiAmt) {
       amount += currencyUnit[currPrice];
       changeSum -= currencyUnit[currPrice];
       crSum -= currencyUnit[currPrice];
     }
 
     if (amount !== 0) {
-      change.push([currPrice, amount]);
+      change.push([currPrice, amount / 100]);
     }
   });
+
+
   if (changeSum > 0) {
      status  = "INSUFFICIENT_FUNDS";
      change = []
-  } else if (changeSum == 0 && changeSumCheck == crSum) {
+  } else if (changeSum == 0 && changeSum == crSum) {
     status = "CLOSED";
     change = cid;
   } else {
     status = "OPEN"
   }
+
+  console.log(status, change);
 
   return {
     status: status,
@@ -92,26 +100,44 @@ function checkCashRegister(price, cash, cid) {
   }
 }
 
-checkCashRegister(19.5, 20, [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100],
-]);
+// checkCashRegister(19.5, 20, [
+//   ['PENNY', 1.01],
+//   ['NICKEL', 2.05],
+//   ['DIME', 3.1],
+//   ['QUARTER', 4.25],
+//   ['ONE', 90],
+//   ['FIVE', 55],
+//   ['TEN', 20],
+//   ['TWENTY', 60],
+//   ['ONE HUNDRED', 100],
+// ]);
 
-console.log(checkCashRegister(19.5, 20, [
-  ['PENNY', 1.01],
-  ['NICKEL', 2.05],
-  ['DIME', 3.1],
-  ['QUARTER', 4.25],
-  ['ONE', 90],
-  ['FIVE', 55],
-  ['TEN', 20],
-  ['TWENTY', 60],
-  ['ONE HUNDRED', 100],
-]));
+
+
+// checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
+
+// should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+
+// console.log("object");
+
+
+
+// checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
+
+// should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+
+// console.log("object");
+
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+
+// should return {status: "INSUFFICIENT_FUNDS", change: []}.
+// console.log("object");
+
+// checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+
+// should return {status: "INSUFFICIENT_FUNDS", change: []}.
+// console.log("object");
+
+// checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+
+// should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
