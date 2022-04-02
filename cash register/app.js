1ebee54331cff3b992c642ea35ad78ce0b9e34ab
Agba Emmanuel <agbaayoh@gmail.com>
2022-04-01 20:23:17 -0700

    cash register algorithm sloved

--- cash register/app.js
+++ cash register/app.js
@@ -22,10 +22,6 @@
 // Twenty Dollars	$20 (TWENTY)
 // One-hundred Dollars	$100 (ONE HUNDRED)
 
-
-
-
-
 // checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return an object.
 
 // checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
@@ -38,53 +34,84 @@
 
 // checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
 
-
 // array.sort(function(a, b) {
 //   return a[1] - b[1];
 // })
 
-
-
 const currencyUnit = {
-  "PENNY": 0.01,
-  "NICKEL": 0.05,
-  "DIME": 0.1,
-  "QUARTER": 0.25,
-  "ONE": 1,
-  "FIVE": 5,
-  "TEN": 10,
-  "TWENTY": 20,
-  "ONE HUNDRED": 100
-}
-
+  PENNY: 0.01,
+  NICKEL: 0.05,
+  DIME: 0.1,
+  QUARTER: 0.25,
+  ONE: 1,
+  FIVE: 5,
+  TEN: 10,
+  TWENTY: 20,
+  'ONE HUNDRED': 100,
+};
 
 // console.log(currencyUnit["ONE HUNDRED"]);
-
 
 function checkCashRegister(price, cash, cid) {
   let changeSum = cash - price;
   let changeSumCheck = changeSum;
   let change = [];
+  let status = '';
 
   let crSum = 0;
-  let filterCid = cid.filter(cash => cash[1] !== 0).reverse()
-  // console.log(filterCid);
+  let filterCid = cid.filter((cash) => cash[1] !== 0).reverse();
 
-  filterCid.forEach(element => {
-    let currPrice = element[0]
-    crSum += element[1]
+  filterCid.forEach((element) => {
+    let currPrice = element[0];
+    crSum += element[1];
 
-    let amount = 0
-    // while (changeSum >= currencyUnit[currPrice] && crSum > 0) {
-    //   //  amount += currencyUnit[currPrice];
-    //   //  changeSum - currencyUnit[currPrice]
-    //   //   console.log(changeSum);
-        
-    // }
-    change.push[currPrice, amount]
+    let amount = 0;
+    while (changeSum >= currencyUnit[currPrice] && crSum > 0) {
+      amount += currencyUnit[currPrice];
+      changeSum -= currencyUnit[currPrice];
+      crSum -= currencyUnit[currPrice];
+    }
 
-
+    if (amount !== 0) {
+      change.push([currPrice, amount]);
+    }
   });
+  if (changeSum > 0) {
+     status  = "INSUFFICIENT_FUNDS";
+     change = []
+  } else if (changeSum == 0 && changeSumCheck == crSum) {
+    status = "CLOSED";
+    change = cid;
+  } else {
+    status = "OPEN"
+  }
+
+  return {
+    status: status,
+    change: change
+  }
 }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 20, [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100],
+]);
+
+console.log(checkCashRegister(19.5, 20, [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100],
+]));
