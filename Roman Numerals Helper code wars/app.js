efd8da9161bbd19976e05b3ed8947682e0d01edf
Agba Emmanuel <agbaayoh@gmail.com>
2022-04-29 08:45:24 -0700

    code wars class based roman numerial helper

--- Roman Numerals Helper code wars/app.js
+++ Roman Numerals Helper code wars/app.js
@@ -8,7 +8,6 @@
 
 // In this kata 4 should be represented as IV, NOT as IIII (the "watchmaker's four").
 
-
 // Symbol 	Value
 // I 	1
 // IV 	4
@@ -22,6 +21,63 @@
 // RomanNumerals.toRoman(1000); // should return 'M'
 // RomanNumerals.fromRoman('M'); // should return 1000
 
+class RomanNumerals {
+  constructor() {
+    this.lookup = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    };
+  }
+  toRoman(num) {
+    let roman = '';
+    for (let i in this.lookup) {
+      while (num >= this.lookup[i]) {
+        roman += i;
+        num -= this.lookup[i];
+      }
+    }
+    return roman;
+  }
+  fromRoman(roman) {
+    let numerial = [...roman.split('')];
 
+    let total = 0,
+          current,
+          currentValue,
+          next,
+          nextValue;
 
+    for (let i = 0; i < numerial.length; i++) {
+      current = numerial[i];
+      currentValue = this.lookup[current];
+      next = numerial[i + 1];
+      nextValue = this.lookup[next]
 
+      if (currentValue < nextValue) {
+        total -= currentValue
+      } else {
+        total += currentValue
+      }
+
+      
+    }
+
+    return total;
+
+  }
+}
+
+const romanNumerals = new RomanNumerals();
+
+romanNumerals.fromRoman('IV');
