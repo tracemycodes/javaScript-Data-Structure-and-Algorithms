f5bb1760023d7ba4ad952d7f6a1bfa09e3ac3dc8
Agba Emmanuel <agbaayoh@gmail.com>
2022-04-30 10:43:58 -0700

    sort out solution to kata

--- Recover a secret string from random triplets code wars/app.js
+++ Recover a secret string from random triplets code wars/app.js
@@ -9,45 +9,80 @@
 // You can assume nothing about the triplets given to you other than that they are valid triplets and that they contain sufficient information to deduce the original string. In particular, this means that the secret string will never contain letters that do not occur in one of the triplets given to you.
 
 
-let secret1 = "whatisup"
-let triplets1 = [
-  ['t','u','p'],
-  ['w','h','i'],
-  ['t','s','u'],
-  ['a','t','s'],
-  ['h','a','p'],
-  ['t','i','s'],
-  ['w','h','s']
-]
+// let secret1 = "whatisup"
+// let triplets1 = [
+//   ['t','u','p'],
+//   ['w','h','i'],
+//   ['t','s','u'],
+//   ['a','t','s'],
+//   ['h','a','p'],
+//   ['t','i','s'],
+//   ['w','h','s']
+// ]
 
 
-var recoverSecret = function(triplets) {
-  let tripletsArr = [...triplets]
-  let newArr = [];
-  tripletsArr.forEach(item => {
+// var recoverSecret = function(triplets) {
+//   let tripletsArr = [...triplets]
+//   let newArr = [];
+//   tripletsArr.forEach(item => {
 
-    for (let i = item.length - 1; i >= 0; i--) {
+//     for (let i = item.length - 1; i >= 0; i--) {
 
-      let current = item[i],
-          prev = item[i + 1],
-          next = item[i - 1];
+//       let current = item[i],
+//           prev = item[i + 1],
+//           next = item[i - 1];
           
           
-      if (!newArr.includes(current) && prev === undefined) {
-        newArr.unshift(current)
-      }
-      if (newArr.indexOf(current) < 0) {
-        newArr = [...newArr.slice(0, newArr.indexOf(prev)), current, ...newArr.slice(newArr.indexOf(prev), newArr.length)]
-      }
+//       if (!newArr.includes(current) && prev === undefined) {
+//         newArr.unshift(current)
+//       }
+//       if (newArr.indexOf(current) < 0) {
+//         newArr = [...newArr.slice(0, newArr.indexOf(prev)), current, ...newArr.slice(newArr.indexOf(prev), newArr.length)]
+//       }
       
-      if ( newArr.indexOf(current) < newArr.indexOf(next)) {
-        newArr.splice(newArr.indexOf(current), 1)
-        newArr = [...newArr.slice(0, newArr.indexOf(prev)), current, ...newArr.slice(newArr.indexOf(prev), newArr.length) ]
-      }     
+//       if ( newArr.indexOf(current) < newArr.indexOf(next)) {
+//         newArr.splice(newArr.indexOf(current), 1)
+//         newArr = [...newArr.slice(0, newArr.indexOf(prev)), current, ...newArr.slice(newArr.indexOf(prev), newArr.length) ]
+//       }     
   
-    }
-  })
-  return newArr.join('')
+//     }
+//   })
+//   return newArr.join('')
+// }
+
+// recoverSecret(triplets1);
+
+
+const recoverSecret = (triplets) => {
+  let duplets = [];
+  let res = '';
+
+  /*
+    convert triplets to duplets
+   */
+  triplets.forEach(trip => {
+    let str = trip.join('');
+    let d1 = str.substr(0,2);
+    let d2 = str.substr(1,3);
+    if (duplets.indexOf(d1) === -1) { duplets.push(d1); } 
+    if (duplets.indexOf(d2) === -1) { duplets.push(d2); }
+  });
+  
+  let nxt = findNext(duplets);
+  while (nxt) {
+    res += nxt;
+    duplets = duplets.filter(d => d.indexOf(nxt) === -1);
+    nxt = findNext(duplets);
+  }
+  
+  return res;
 }
 
-recoverSecret(triplets1);
+/**
+ * Return the first letter of the secret word.
+ */
+function findNext(duplets) {
+  // 1st char is not the 2nd char in any duplet
+  let nxt = duplets.find(d => duplets.every(_d => d[0] !== _d[1])); 
+  return duplets.length > 1 ? nxt[0] : duplets[0];
+}
