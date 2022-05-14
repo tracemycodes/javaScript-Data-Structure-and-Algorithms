813f92c3f7bcec4feb4042c433192cf480032990
Agba Emmanuel <agbaayoh@gmail.com>
2022-05-14 07:54:58 -0700

    ip address count kata

--- count ip addresses code wars/app.js
+++ count ip addresses code wars/app.js
@@ -4,11 +4,25 @@
 
 // All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
 
-// * With input "10.0.0.0", "10.0.0.50"  => return   50 
-// * With input "10.0.0.0", "10.0.1.0"   => return  256 
+// * With input "10.0.0.0", "10.0.0.50"  => return   50
+// * With input "10.0.0.0", "10.0.1.0"   => return  256
 // * With input "20.0.0.10", "20.0.1.0"  => return  246
 
+function ipsBetween(start, end) {
+  let startArr = [...start.split('.')].map((x) => parseInt(x));
+  let endArr = [...end.split('.')].map((x) => parseInt(x));
 
-// function ipsBetween(start, end){
-//   //TODO
-// }
+  let ansArr = Math.abs(
+    startArr
+      .map((x, index) =>
+        x == endArr[index]
+          ? 0
+          : (x - endArr[index]) * Math.pow(256, endArr.length - 1 - index)
+      )
+      .reduce((a, b) => a + b)
+  );
+
+  return ansArr;
+}
+
+console.log(ipsBetween('10.11.12.13', '10.11.13.0'));
