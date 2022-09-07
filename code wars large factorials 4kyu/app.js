463b8b85427a52b3211f94cc43cd1f5265b994b5
Agba Emmanuel <agbaayoh@gmail.com>
2022-09-07 07:01:08 -0700

    large factorials

---
+++ code wars large factorials 4kyu/app.js
@@ -0,0 +1,43 @@
+// Large Factorials
+
+// In mathematics, the factorial of integer n is written as n!. It is equal to the product of n and every integer preceding it. For example: 5! = 1 x 2 x 3 x 4 x 5 = 120
+
+// Your mission is simple: write a function that takes an integer n and returns the value of n!.
+
+// You are guaranteed an integer argument. For any values outside the non-negative range, return null, nil or None (return an empty string "" in C and C++). For non-negative numbers a full length number is expected for example, return 25! =  "15511210043330985984000000" as a string.
+
+// For more on factorials, see http://en.wikipedia.org/wiki/Factorial
+
+// NOTES:
+
+//     The use of BigInteger or BigNumber functions has been disabled, this requires a complex solution
+
+//     I have removed the use of require in the javascript language.
+
+// function add(a, b) {
+//   var res = '',
+//     c = 0;
+//   a = a.split('');
+//   b = b.split('');
+//   while (a.length || b.length || c) {
+//     c += ~~a.pop() + ~~b.pop();
+//     res = (c % 10) + res;
+//     c = c > 9;
+//   }
+//   return res;
+// }
+
+function factorial(n) {
+  let ans = 0;
+
+  for (i = 1; i <= n; i++) {
+    console.log('object', i);
+    for (j = 1; j <= i; j++) {
+      console.log(i);
+    }
+  }
+
+  return ans.toString();
+}
+
+console.log(factorial(150));
