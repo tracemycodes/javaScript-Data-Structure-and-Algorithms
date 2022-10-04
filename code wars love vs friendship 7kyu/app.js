f7643d3ab87b7c1bec485f13fa5012a411bb2acc
Agba Emmanuel <agbaayoh@gmail.com>
2022-10-04 23:27:19 +0100

    love vs friendship kata

---
+++ code wars love vs friendship 7kyu/app.js
@@ -0,0 +1,26 @@
+
+// Love vs friendship
+// 1106092% of 1,7784,898 of 12,253J or nor J
+//  JavaScript
+// Node v14.x
+// VIM
+// EMACS
+// Instructions
+// Output
+// If　a = 1, b = 2, c = 3 ... z = 26
+
+// Then l + o + v + e = 54
+
+// and f + r + i + e + n + d + s + h + i + p = 108
+
+// So friendship is twice as strong as love :-)
+
+// Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+
+// The input will always be made of only lowercase letters and will never be empty.
+
+function wordsToMarks(string){
+    //your code here
+}
+
+wordsToMarks("attitude")
